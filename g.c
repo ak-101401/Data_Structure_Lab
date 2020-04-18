c89c0c712962c383a122eb00527bb5505e222342
ak-101401 <54812001+ak-101401@users.noreply.github.com>
2020-04-18 16:47:45 +0530

    Create g.c

---
+++ g.c
@@ -0,0 +1,68 @@
+//By ADITYA KALRA, IT, 11912038	
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct node
+{
+    int data;
+    struct node* lchild;
+    struct node* rchild;
+}node;
+
+node* create()
+{
+    int y;
+    printf ("Enter data=");
+    scanf("%d",&y);
+    if (y==-1)
+    return NULL;
+    node* temp=(node*)malloc(sizeof(node));
+    temp->rchild=temp->lchild=NULL;
+    temp->data=y;
+    printf ("To enter left child of %d press 1\n",temp->data);
+    scanf("%d",&y);
+    if (y==1)
+    temp->lchild=create();
+    printf ("To enter right child of %d press 1\n",temp->data);
+    scanf("%d",&y);
+    if (y==1)
+    temp->rchild=create();
+    return temp;
+}
+int lnodes(node* root)
+{
+    if (!root)
+    return 0;
+    if (!root->rchild && !root->lchild)
+    return 1;
+    return lnodes(root->lchild)+lnodes(root->rchild);
+}
+int nlnodes(node* root)
+{
+    if (!root)
+    return 0;
+    if (!root->rchild && !root->lchild)
+    return 0;
+    return 1+nlnodes(root->lchild)+nlnodes(root->rchild);
+}
+int main()
+{
+    node* root=create();
+    printf ("Number of leaf nodes= %d\n",lnodes(root));
+    printf ("Number of non-leaf nodes= %d\n",nlnodes(root));
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//By ADITYA KALRA
